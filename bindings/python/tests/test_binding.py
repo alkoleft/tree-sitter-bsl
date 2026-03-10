f71b99ae1514e69bb75138ed8364c0d7d69ed518
alkoleft <alkoleft@gmail.com>
2026-03-11 00:16:06 +0300

    fix(python): use Parser(language) constructor instead of removed set_language() for tree-sitter>=0.25

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -7,8 +7,8 @@ class TestLanguage(unittest.TestCase):
     def test_can_load_grammar(self):
         language = tree_sitter_bsl.Language()
         self.assertIsNotNone(language)
-        parser = tree_sitter.Parser()
-        parser.set_language(language)
+        parser = tree_sitter.Parser(language)
+        self.assertIsNotNone(parser)
         self.assertEqual(language.abi_version, tree_sitter.Language.abi_version)
 
 
