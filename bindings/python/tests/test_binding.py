0a4c7deb6f2f7c400b74a9923d2632151c407d65
alkoleft <alkoleft@gmail.com>
2026-03-11 00:06:51 +0300

    test(python): verify grammar compatibility with py-tree-sitter in test_can_load_grammar

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -1,4 +1,5 @@
 import unittest
+import tree_sitter
 import tree_sitter_bsl
 
 
@@ -6,6 +7,9 @@ class TestLanguage(unittest.TestCase):
     def test_can_load_grammar(self):
         language = tree_sitter_bsl.Language()
         self.assertIsNotNone(language)
+        parser = tree_sitter.Parser()
+        parser.set_language(language)
+        self.assertEqual(language.abi_version, tree_sitter.Language.abi_version)
 
 
 if __name__ == "__main__":
